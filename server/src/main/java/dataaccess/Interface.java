4979774cd95b4aefb1bba75ac78ea09484cbf5e9
Connor Ohran <connor.ohran@gmail.com>
2025-07-12 22:38:49 -0600

    Initiated making models and interface, kinda confused tbh

---
+++ server/src/main/java/dataaccess/Interface.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+public class Interface {
+    //map maybe
+
+    public void registerPlayer() {
+
+    }
+
+    public void loginPlayer() {
+
+    }
+
+    public void logoutPlayer() {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void createGame() {
+
+    }
+
+    public void joinGame() {
+
+    }
+
+    public void deleteAllGames() {
+        games.clear();
+    }
+}
