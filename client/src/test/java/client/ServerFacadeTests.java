e2874936834d07a791c649dc7868657ec82d41f1
Connor Ohran <connor.ohran@gmail.com>
2025-07-30 22:48:21 -0600

    Login tests completed

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,6 +4,7 @@ import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.ServerFacade;
+import service.requests.LoginRequest;
 import service.requests.RegisterRequest;
 import service.results.RegisterResult;
 
@@ -55,8 +56,21 @@ public class ServerFacadeTests {
 
         RegisterRequest request2 = new RegisterRequest("Bob", "BobsPass", "Bobby1@gmail.com");
 
-
         assertThrows(DataAccessException.class, () -> facade.registerUser(request2));
+    }
+
+    @Test
+    public void loginUserPositive() throws DataAccessException {
+        facade.registerUser(new RegisterRequest("Hannah", "password123", "hannah@gmail.com"));
+        LoginRequest request = new LoginRequest("Hannah", "password123");
+        assertNotNull(facade.login(request));
+    }
+
+    @Test
+    public void loginUserNegative() throws DataAccessException {
+        facade.registerUser(new RegisterRequest("Hannah", "password123", "hannah@gmail.com"));
+        LoginRequest request = new LoginRequest("Hannah", "password1234");
+        assertThrows(DataAccessException.class, () -> facade.login(request));
     }
 
 }
